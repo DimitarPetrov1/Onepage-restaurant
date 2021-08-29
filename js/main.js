15c1997987dfa4efc0fb1bc159a98bda8c097280
Dimitar Petrov <dimitarpetrov1990@gmail.com>
2021-08-29 18:36:32 +0300

    added mbile menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,47 @@
-const links = document.querySelectorAll('a');
+const links = document.querySelectorAll("a");
 let reservationFormOpened = false;
-const resetvationForm = document.querySelector('reservation-form');
-const reservationFormCloseBtn = document.getElementById('closeBtn');
+let menuOpen = false;
+const resetvationForm = document.querySelector(".reservation-form");
+const reservationButton = document.getElementById("reservationButton");
+const reservationFormCloseBtn = document.getElementById("closeBtn");
+const menuBtn = document.getElementById("menuBtn");
+const mobileNav = document.querySelector(".nav-mobile");
 
-links.forEach(link=> {
-    link.addEventListener('click', (a)=>{
-        a.preventDefault()
-    })
-})
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (menuOpen) {
+      mobileNav.style.visibility = "hidden";
+      menuOpen = false;
+    }
+  });
+});
+
+reservationButton.addEventListener("click", () => {
+  if (!reservationFormOpened) {
+    resetvationForm.style.visibility = "visible";
+    reservationFormOpened = true;
+  }
+});
+reservationFormCloseBtn.addEventListener("click", () => {
+  if (reservationFormOpened) {
+    resetvationForm.style.visibility = "hidden";
+    reservationFormOpened = false;
+  }
+});
+// scroll to location
+function scrollToLocation(target) {
+  let Target = document.querySelector(target).offsetTop;
+  window.scrollTo(0, Target);
+}
+
+// the mobile nav
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    mobileNav.style.visibility = "visible";
+    menuOpen = true;
+  } else {
+    mobileNav.style.visibility = "hidden";
+    menuOpen = false;
+  }
+});
